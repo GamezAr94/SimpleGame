a7698d2f3a58f08a3b05a44ec6ae65a2d5b65b4f
GamezArt94 <46079765+GamezAr94@users.noreply.github.com>
2019-12-20 05:55:01 -0800

    player collider
    Adding a script to the player that detects all collisions

---
+++ SimpleGame/Assets/Scripts/Collider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollider : MonoBehaviour {
+
+    public PlayerMovement script;
+
+	void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            script.enabled = false;
+        }
+    }
+}
