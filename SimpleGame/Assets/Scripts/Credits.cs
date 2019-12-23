7a7ecd1c7aac222ba69b2b588680c25936f72390
GamezArt94 <46079765+GamezAr94@users.noreply.github.com>
2019-12-23 08:10:23 -0800

    Creating the UI
    adding the menu and the credits screen, adding some animations to the UI

---
+++ SimpleGame/Assets/Scripts/Credits.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit!");
+    }
+
+
+    public void StartGame()
+    {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
